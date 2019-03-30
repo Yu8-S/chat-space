4463859ee7c2894b313607161658cd029c2e61f8
Yuya <yu8.0101@icloud.com>
2019-03-30 18:40:55 +0900

    Update message.js
    JSONを受け取りHTMLを作成する為にbuildHTMLメソッドを定義する。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,15 @@
 $(function() {
+  function buildHTML(message) {
+    var html = `%p.chatarea-main__user__name
+                 = message.user.name
+                   %p.chatarea-main__user__date
+                     = message.created_at.strftime("%Y/%m/%d %H:%M")
+                   %p.chatarea-main__text
+                     - if message.body.present?
+                       = message.body
+                       = image_tag message.image.url if message.image.present?`
+  }
+
   $('#new_message').on('submit', function(e) {
     e.preventDefaul();
     var formData = new FormData(this);

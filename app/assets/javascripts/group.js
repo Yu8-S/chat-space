434a2c7df7156f2e27924dbcb6f86749ba742206
Yuya <yu8.0101@icloud.com>
2019-04-01 21:16:45 +0900

    Update group.js
    .doneにて返ってきたjsonデータを受け取り、条件一致したユーザー名を表示するhtmlを追加。
    非同期通信失敗時のアラートを追加。

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -1,11 +1,32 @@
 $(function() {
-  $("#user-search-field").keydown(function() {
+  var search = $('#user-search-result');
+  function buildHTML(user) {
+  var html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${ user.name }</p>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${ user.id } data-user-name=${  user.name}>追加</a>
+              </div>`
+    search.append(html)
+  }
+
+  $("#user-search-field").on('keyup', function() {
     var input = $(this).val();
     $.ajax({
       type: 'GET',
       url: '/users',
-      date: {keyword: input},
-      dataType: 'json'
+      data: { keyword: input },
+      dataType: 'json',
+    })
+
+    .done(function(data) {
+      $("#user-search-result").empty();
+      if (data.length !== 0) {
+        data.forEach(function(html) {
+          buildHTML(html);
+        });
+      }
+    })
+    .fail(function(data) {
+      alert('非同期処理に失敗しました');
     })
   })
 });

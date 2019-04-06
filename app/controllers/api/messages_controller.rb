d094165576c2e646b61fc533e547b3e880729136
Yuya <yu8.0101@icloud.com>
2019-04-06 13:34:37 +0900

    Update messages_controller.rb
    Idを比較するタイミングを修正。

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,5 +1,8 @@
 class Api::MessagesController < ApplicationController
   def index
-    @messages = Message.where(('id > ?', params[:message_id]))
+    @messages = Message.all
+    respond_to do |format|
+        format.html
+        format.json { @new_message = Message.where('id <= ?', params[:message][:id])}
   end
 end

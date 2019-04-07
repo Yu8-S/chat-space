93d75c7fc3d7e938e810185ff82a3047f6cb4df7
Yuya <yu8.0101@icloud.com>
2019-04-07 19:02:05 +0900

    Update messages_controller.rb
    該当するグループのデータのみ取得するように修正。

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,8 +1,10 @@
 class Api::MessagesController < ApplicationController
   def index
-    @messages = Message.all
+    @group = Group.find(params[:group_id])
+    @messages = @group.messages.includes(:user)
+    @new_messages = @messages.where("id > ?", params[:id])
     respond_to do |format|
-        format.html
-        format.json { @new_message = Message.where('id <= ?', params[:message][:id])}
+      format.json
+    end
   end
 end

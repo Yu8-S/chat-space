9891fbedffd7d911bdc569375220d3719d501a12
Yuya <yu8.0101@icloud.com>
2019-04-07 19:02:47 +0900

    Update group.js
    余分なスペースを削除。

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -4,7 +4,7 @@ $(function() {
   function buildHTML(user) {
   var html = `<div class="chat-group-user clearfix">
                 <p class="chat-group-user__name">${ user.name }</p>
-                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${ user.user_id } data-user-name=${  user.name}>追加</a>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${ user.user_id } data-user-name=${ user.name}>追加</a>
               </div>`
     search.append(html)
   }

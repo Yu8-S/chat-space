28e272caa6bb90bc1ead5e7a6e0cdb575df67f90
Yuya <yu8.0101@icloud.com>
2019-04-07 19:01:15 +0900

    Update Message.js
    自動更新時、エラーが出るため修正。
    
    更新後、HTML表示が崩れていたため、修正。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function() {
   function buildHTML(message) {
-    var html = `<div class=chatarea-main__user id=chatarea-main__user data-id='${ message.id }'>
+    var html = `<div class=chatarea-main__user data-id=${message.id}>
                   <p class=chatarea-main__user__name>${message.user_name}</p>
                   <p class=chatarea-main__user__date>${message.created_at}</p>
                 </div>
@@ -9,54 +9,11 @@ $(function() {
   }
 
   var buildMessageHTML = function(message) {
-    if (message.body && message.image.url) {
-      var html = `<div class="message" data-id='${message.id}'>
-        <div class="upper-message">'
-          <div class="upper-message__user-name">
-            ${message.user_name}
-          </div>
-          <div class="upper-message__date">
-            ${message.created_at}
-          </div>
-        </div>
-        <div class="lower-message">
-          <p class="lower-message__content">
-            ${message.body}
-          </p>
-          <img src="'${message.image.url}'" class="lower-message__image" >
-        </div>
-      </div>`
-    } else if (message.body) {
-      var html = `<div class="message" data-id='${message.id}'>
-        <div class="upper-message">
-          <div class="upper-message__user-name">
-            ${message.user_name}
-          </div>
-          <div class="upper-message__date">
-            ${message.created_at}
-          </div>
-        </div>
-        <div class="lower-message">
-          <p class="lower-message__content">
-            ${message.body}
-          </p>
-        </div>
-      </div>`
-    } else if (message.image.url) {
-      var html = `<div class="message" data-id='${message.id}>
-        <div class="upper-message">
-          <div class="upper-message__user-name">
-            ${message.user_name}
-          </div>
-          <div class="upper-message__date">
-            ${message.created_at}
-          </div>
-        </div>
-        <div class="lower-message">
-          <img src='${message.image.url}' class=lower-message__image >
-        </div>
-      </div>`
-    };
+    var html = `<div class=chatarea-main__user data-id=${message.id}>
+                  <p class=chatarea-main__user__name>${message.user_name}</p>
+                  <p class=chatarea-main__user__date>${message.created_at}</p>
+                </div>
+                <p class=chatarea-main__text>${message.body}</p>`
     return html;
   };
 
@@ -78,35 +35,28 @@ $(function() {
     })
     .done(function(data) {
       var html = buildHTML(data);
-      console.log(html);
       $('.chatarea-main').append(html)
       $('#new_message')[0].reset()
       $('.chatarea-main').animate({ scrollTop:$('.chatarea-main')[0].scrollHeight });
-      return false
     })
     .fail(function(date) {
       alert('非同期処理に失敗しました');
     })
   })
 
-  var reloadMessages = function() {
-    var last_message_id = $('#chatarea-main__user:last').data('id');
-    console.log(last_message_id);
+  function reloadMessages() {
+    var last_message_id = $('.chatarea-main__user:last').data('id')
+    var href = location.href.replace('/messages', '/api/messages')
     $.ajax({
-      url: location.href,
-      type: 'get',
-      dataType: 'json',
-      data: {
-        message: { id: last_message_id }
-      }
+      url: href,
+      type: 'GET',
+      data: {id: last_message_id},
+      dataType: 'json'
     })
-    .done(function(messages) {
-      var insertHTML = '';
-      $.each(messages, function(insertHTML, messsage) {
-        var html = buildMessageHTML(insertHTML);
+    .done(function(data) {
+      $.each(data, function(i, data) {
+        var html = buildMessageHTML(data);
         $('.chatarea-main').append(html)
-        $('.chatarea-main').animate({ scrollTop:$('.chatarea-main')[0].scrollHeight });
-      return false
       })
     })
     .fail(function(XMLHttpRequest, textStatus, errorThrown) {
@@ -114,7 +64,8 @@ $(function() {
       console.log("XMLHttpRequest : " + XMLHttpRequest.status);
       console.log("textStatus     : " + textStatus);
       console.log("errorThrown    : " + errorThrown.message);
-    });
-  };
-  setInterval(reloadMessages, 20000);
+    })
+  }
+
+  setInterval(reloadMessages, 5000);
 });
